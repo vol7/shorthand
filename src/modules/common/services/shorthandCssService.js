69813722e2f878831d58b27bd3c5bdbb969437a5
Francis Marineau <frank.marineau@gmail.com>
2014-08-02 19:05:05 -0400

    Per-shorthand replacement logic + fix font

--- src/modules/common/services/shorthandCssService.js
+++ src/modules/common/services/shorthandCssService.js
@@ -4,30 +4,61 @@ module.exports = /*@ngInject*/
   function shorthandCssService() {
     var css = require('css');
 
+    var basicShorthandReplace = function(shorthand, declarations) {
+      var shorthandValue = shorthand.shorthandSyntax;
+
+      shorthand.properties.forEach(function(longProperty) {
+        if (declarations[longProperty]) {
+          shorthandValue = shorthandValue.replace(longProperty, declarations[longProperty].value);
+        }
+        else {
+          shorthandValue = shorthandValue.replace(longProperty, '');
+        }
+      });
+
+      shorthandValue = shorthandValue.replace('  ', ' ').trim();
+      return shorthandValue;
+    };
+
     var shorthands = [{
       shorthandProperty: 'background',
       properties: ['background-color', 'background-image', 'background-repeat', 'background-position', 'background-attachment'],
-      shorthandSyntax: 'background-color background-image background-repeat background-position background-attachment'
+      shorthandSyntax: 'background-color background-image background-repeat background-position background-attachment',
+      getShorthandValue: basicShorthandReplace
     }, {
       shorthandProperty: 'font',
       properties: ['font-style', 'font-variant', 'font-weight', 'font-size', 'line-height', 'font-family'],
-      shorthandSyntax: 'font-style font-variant font-weight font-size/line-height font-family'
+      shorthandSyntax: 'font-style font-variant font-weight font-size/line-height font-family',
+      getShorthandValue: function(shorthand, declarations) {
+        var shorthandValue = basicShorthandReplace(shorthand, declarations);
+
+        // We must at least have size & family or else the shorthand will get ignored by the browser
+        if (!declarations['font-size'] || !declarations['font-family']) {
+          return "";
+        }
+
+        // Remove slash if line-height is not specified
+        if (!declarations['line-height']) {
+          shorthandValue = shorthandValue.replace('/', '');
+        }
+
+        return shorthandValue.replace('  ', ' ').trim();
+      }
     }, {
       shorthandProperty: 'border',
       properties: ['border-width', 'border-style', 'border-color'],
-      shorthandSyntax: 'border-width border-style border-color'
-    }, {
-      shorthandProperty: 'border',
-      properties: ['border-width', 'border-style', 'border-color'],
-      shorthandSyntax: 'border-width border-style border-color'
+      shorthandSyntax: 'border-width border-style border-color',
+      getShorthandValue: basicShorthandReplace
     }, {
       shorthandProperty: 'outline',
       properties: ['outline-width', 'outline-style', 'outline-color'],
-      shorthandSyntax: 'outline-width outline-style outline-color'
+      shorthandSyntax: 'outline-width outline-style outline-color',
+      getShorthandValue: basicShorthandReplace
     }, {
       shorthandProperty: 'list-style',
       properties: ['list-style-type', 'list-style-position', 'list-style-image'],
-      shorthandSyntax: 'list-style-type list-style-position list-style-image'
+      shorthandSyntax: 'list-style-type list-style-position list-style-image',
+      getShorthandValue: basicShorthandReplace
     }];
 
     return function(cssString) {
@@ -35,28 +66,16 @@ module.exports = /*@ngInject*/
 
       cssObject.stylesheet.rules.forEach(function(rule) {
         var declarations = [];
+        console.log(rule);
 
         rule.declarations.forEach(function(declaration) {
           declarations[declaration.property] = declaration;
         });
 
         shorthands.forEach(function(shorthand) {
-          var longPropertiesUsed = false;
-          var shorthandValue = shorthand.shorthandSyntax;
+          var shorthandValue = shorthand.getShorthandValue(shorthand, declarations);
 
-          shorthand.properties.forEach(function(longProperty) {
-            if (declarations[longProperty]) {
-              longPropertiesUsed = true;
-              shorthandValue = shorthandValue.replace(longProperty, declarations[longProperty].value);
-            }
-            else {
-              shorthandValue = shorthandValue.replace(longProperty, '');
-            }
-          });
-
-          shorthandValue = shorthandValue.replace('  ', ' ').trim();
-
-          if (longPropertiesUsed) {
+          if (shorthandValue !== "") {
             var newDeclarations = [];
 
             // Add the new shorthand property
